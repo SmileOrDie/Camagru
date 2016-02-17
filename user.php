4f229b397eafddfc7329ed355b490edfe67f7ef7
Smail HAMDANI <shamdani@e2r1p13.42.fr>
2016-02-17 14:39:56 +0100

    avant reset session

--- user.php
+++ user.php
@@ -1,8 +1,24 @@
 <?php
-		var_dump($_POST);
-		echo "<br>";
-		if (empty($_POST['name']) == FALSE)
-			echo "YES FORMULAIRE POST";
-		else
-			print_r($_SERVER['HTTP_REFERER']);
+	// var_dump($_POST);
+	echo "<br>";
+	if (empty($_POST['name']) == FALSE)
+		echo "YES FORMULAIRE POST";
+	else
+	{
 ?>
+<html>
+<head>
+	<title>Camagru</title>
+	<link rel="stylesheet" type="text/css" href="front.css">
+</head>
+<body style="background-color:darkgrey;">
+	<div id="retour">
+		<form action="<?php echo $_SERVER['HTTP_REFERER'];?>"  method="post">
+			<input type="submit" value="Retour">
+		</form>
+	</div>
+</body>
+<?php
+	}
+?>
+</html>
